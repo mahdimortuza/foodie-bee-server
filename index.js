57479b03b300d2aac9f79a5d220893742092cd91
mahdimortuza <mahdi.webx@gmail.com>
2024-02-29 22:11:22 +0600

    all done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({origin: "https://foodie-bee-client-main.vercel.app", credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
